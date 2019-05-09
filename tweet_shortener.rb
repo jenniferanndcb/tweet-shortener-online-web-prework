6f27c9d4eaaa2ac8610edbcc06c14c3f54450f35
Jen <jenniferanndcb@gmail.com>
2019-05-09 15:02:22 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,15 +18,23 @@ end
   
 
 def word_substituter(tweet)
-  split_tweet = tweet.split(" ")
-
-   split_tweet.each_with_index do |word, index|
-    dictionary.keys.each do |dict|
-      if word == dict || word == dict.capitalize
-        split_tweet.delete_at(index)
-        split_tweet.insert(index, dictionary[dict])
-      end
+  array = []
+  split_tweet = tweet.split
+  split_tweet.each do |word|
+    if dictionary[word.downcase]
+      word = dictionary[word.downcase]
+      array << word
+    else
+      array << word
     end
   end
-  split_tweet.join(" ")
+  array.join(" ")
+end
+
+
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    puts word_substituter(tweet)
+  end
 end
